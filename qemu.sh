7c71223bdfa7cf0da6a9a4a7d378449efd8cf2ea
Jakub Latusek <jk.latusek@gmail.com>
2025-12-04 09:46:46 +0100

    qemu: set proper boot options to make ssh works

--- qemu.sh
+++ qemu.sh
@@ -8,6 +8,4 @@ qemu-system-arm \
     -drive file=output/images/rootfs.ext2,if=scsi,format=raw \
     -append "rootwait root=/dev/sda console=ttyAMA0,115200" \
     -serial stdio \
-    -net nic,model=rtl8139 -net user \
-    -netdev user,id=n1,hostfwd=tcp::2222-:22 \
-    -device virtio-net-pci,netdev=n1
+    -net nic,model=rtl8139 -net user,hostfwd=tcp::2222-:22
