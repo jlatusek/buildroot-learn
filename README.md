7871c0193408cfafeea48181a0cf5e05b07a62c9
Jakub Latusek <jk.latusek@gmail.com>
2025-12-04 10:09:01 +0100

    ssh: auth by key

---
+++ README.md
@@ -0,0 +1,16 @@
+# Setup
+
+Add this configuration to `~/.ssh/config`
+
+
+```
+``` sh
+Host localhost
+   HostName localhost
+   UserKnownHostsFile /dev/null
+   StrictHostKeyChecking=no
+```
+```
+
+
+
--- qemu.sh
+++ qemu.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+BUILD_ROOT_DIR=$(dirname $(realpath $BASH_SOURCE[0]))
+
+if [ ! -f ${BUILD_ROOT_DIR}/board/custom/rootfs_overlay/root/.ssh/authorized_keys ]; then
+    cp ~/.ssh/id_rsa.pub ${BUILD_ROOT_DIR}/board/custom/rootfs_overlay/root/.ssh/authorized_keys
+fi
+
 qemu-system-arm \
     -M versatilepb \
     -m 256 \
