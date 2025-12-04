8c0d8123eb6e34b8ca7d9686f1beaa5c7f768fcf
Jakub Latusek <jk.latusek@gmail.com>
2025-12-04 09:16:13 +0100

    use proper qemu option to log boot msg and to have internet access

--- qemu.sh
+++ qemu.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
+
 qemu-system-arm \
     -M versatilepb \
     -m 256 \
     -kernel output/images/zImage \
     -dtb output/images/versatile-pb.dtb \
-    -hda output/images/rootfs.ext2 \
-    -append "root=/dev/sda console=/dev/ttyAMA0,115200" \
-    -nographic
+    -drive file=output/images/rootfs.ext2,if=scsi,format=raw \
+    -append "rootwait root=/dev/sda console=ttyAMA0,115200" \
+    -serial stdio \
+    -net nic,model=rtl8139 -net user \
+    -netdev user,id=n1,hostfwd=tcp::2222-:22 \
+    -device virtio-net-pci,netdev=n1
